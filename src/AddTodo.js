87e1c7da0095008c65112362ad18620db2aa819f
Lee Ji Min <vahksjdf@sookmyung.ac.kr>
2023-04-17 17:52:18 +0900

    10. React로 목록 추가, 수정, 삭제하기

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -5,7 +5,7 @@ class AddTodo extends React.Component {
     constructor(props){ 
         super(props); 
         this.state ={item :{title:""}}; 
-        this.add = props.add;  // props 의 함수를 this.add 에 연결, props에는 상위 컴포넌트(App.js)의 함수, 매개변수가 들어 있음. 
+        this.add = props.add; // props 의 함수를 this.add 에 연결, props에는 상위 컴포넌트(App.js)의 함수, 매개변수가 들어 있음. 
     } 
     onInputChange =(e)=>{ 
         const thisItem =this.state.item; 
@@ -13,10 +13,9 @@ class AddTodo extends React.Component {
         this.setState({item:thisItem}); 
         console.log(thisItem); 
     }
-
     onButtonClick =()=>{ 
         this.add(this.state.item); 
-        this.setState({item:{title:""}});  // text 값을 추가하고 입력 필드는 초기화시킨다. 
+        this.setState({item:{title:""}}); // text 값을 추가하고 입력 필드는 초기화시킨다. 
     }
     
     enterKeyEventHandler =(e)=>{ 
@@ -30,7 +29,7 @@ class AddTodo extends React.Component {
             <Grid container>
                 <Grid xs={11} md={11} item style={{paddingRight:16}}>
                     <TextField
-                    placehoder="Add Todo here"
+                    placehoder="Add Todo here" 
                     fullWidth
                     onChange={this.onInputChange}
                     value={this.state.item.title}
@@ -38,18 +37,18 @@ class AddTodo extends React.Component {
                     />
                 </Grid>
                 <Grid xs={1} md={1} item>
-                  <Button
-                  fullWidth
-                  color="secondary"
-                  variant="outlined"
-                  onClick={this.onButtonClick}
-                  >
-                    +
-                  </Button>
+                    <Button
+                    fullWidth
+                    color="secondary"
+                    variant="outlined"
+                    onClick={this.onButtonClick}
+                    >
+                        +
+                    </Button>
                 </Grid>
             </Grid>
           </Paper>
-        ); 
+        );
     }
 }
 
